b66edf7d7e7c032fecc70afdd7eb00fd77f65d8c
Tim Miller <tmiller@eosgsb.com>
2020-06-30 16:30:58 -0500

    removed key/secret, added domainname variable

--- pushIP.py
+++ pushIP.py
@@ -1,40 +1,52 @@
-#!/usr/bin/python3
 from time import sleep
-from datetime import datetime
-import requests
+import requests,logging, datetime,os
 from godaddypy import Client, Account
-from datetime import datetime
 global currentip
 
+
+domain = ""
+
+#Get public IP address
 IPADDR= str(requests.get('http://ip.42.pl/raw').text)
-godaddy_key="dKt55ixCf7cp_EpAtqWabDLagjACMABWqA2"
-godaddy_secret="EpAw5Sr5uCKcKCqKJzwT2J"
 
 
-time = datetime.now()
+#Godaddy keys: https://developer.godaddy.com/getstarted
+godaddy_key=""
+godaddy_secret=""
 
+logging.basicConfig(filename='{}/logs/{}.log'.format(os.getcwd(),datetime.datetime.now().strftime("%m_%d_%Y_%H-%M")),level=logging.DEBUG)
+changed = ''
 
 def check_ip():
+    try:
         godaddyip =get_godaddy_ip()[0]['data']
-        currentip= str(requests.get('http://ip.42.pl/raw').text)
-        print ('current public IP is: {}, godaddy IP is: {}, time is: {}'.format(currentip, godaddyip,time))
+        currentip= IPADDR
+        print ('current public IP is: {}, godaddy IP is: {}'.format(currentip, godaddyip))
         if godaddyip != currentip:
-                print ('updating IP to {}'.format(currentip))
-                update_godaddy_ip(currentip)
+            print ('updating IP to {}'.format(currentip))
+            changed = True
+            update_godaddy_ip(currentip)
+
         else:
-                print ('same IP, no need to update')
+            print ('same IP, no need to update')
+            changed = False
+        logging.info('{} - GoDaddyIP:{} - Public IP:{} - Changed?:{}'.format(datetime.datetime.now(),godaddyip,currentip,changed))
+    except:
+        pass
 
 def update_godaddy_ip(ip):
-        vinylcraft = Account(api_key=godaddy_key,api_secret=godaddy_secret)
-        client = Client(vinylcraft)
-        client.update_ip(ip, domains=['vinylcraft.net'])
+    try:
+        domain = Account(api_key=godaddy_key,api_secret=godaddy_secret)
+        client = Client(domain)
+        client.update_ip(ip, domains=[domain])
+    except:
+        pass
 
 def get_godaddy_ip():
-        vinylcraft = Account(api_key=godaddy_key,api_secret=godaddy_secret)
-        client = Client(vinylcraft)
-        r = client.get_records('vinylcraft.net', record_type='A', name='@')
-        return r
+    vinylcraft = Account(api_key=godaddy_key,api_secret=godaddy_secret)
+    client = Client(vinylcraft)
+    r = client.get_records(domain, record_type='A', name='@')
+    return r
 
 currentip = ''
-
 check_ip()
